40fd352d7a1cdd8da38d28486857195845af1480
rza <cruel.gperla.crew@gmail.com>
2017-10-12 22:53:24 +0200

    mislim da je gotovo

--- app/src/main/java/kuvarica/rade/cookingapp/activity/RecipeDetailsActivity.java
+++ app/src/main/java/kuvarica/rade/cookingapp/activity/RecipeDetailsActivity.java
@@ -1,12 +1,18 @@
 package kuvarica.rade.cookingapp.activity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import kuvarica.rade.cookingapp.R;
 import model.Recept;
@@ -25,7 +31,6 @@ public class RecipeDetailsActivity extends AppCompatActivity {
 
         intent = getIntent();
         String id = intent.getStringExtra("id");
-        Log.d("ID", id);
 
         Recept recept = MainActivity.db.getRecipeById(id);
         tvImeRecepta = (TextView) findViewById(R.id.tv_recept_ime_details);
@@ -40,7 +45,25 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         fabSms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO intent za sms
+                final Dialog d = new Dialog(RecipeDetailsActivity.this);
+                d.setContentView(R.layout.dialog_sms);
+                Button btnSendSms = d.findViewById(R.id.btn_sms_send);
+                final EditText etSmsNumber = d.findViewById(R.id.et_sms_send_number);
+
+                btnSendSms.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String smsContent = tvImeRecepta.getText().toString() + " Instructions: \n" + tvSadrzajRecepta.getText().toString();
+                        SmsManager sms = SmsManager.getDefault();
+                        String number = etSmsNumber.getText().toString();
+
+                        if (!number.equals("+381")) {
+                            sms.sendTextMessage(number, null, smsContent, null, null);
+                            d.cancel();
+                        }
+                        Toast.makeText(RecipeDetailsActivity.this, "Please enter a valid phone number", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
@@ -48,7 +71,28 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         fabEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO intent za email
+                final Dialog d = new Dialog(RecipeDetailsActivity.this);
+                d.setContentView(R.layout.dialog_email);
+                Button btnSendEmail = d.findViewById(R.id.btn_email_send);
+                final EditText etEmail = d.findViewById(R.id.et_email_send);
+
+                btnSendEmail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String email = etEmail.getText().toString();
+
+                        if (!email.equals("")) {
+                            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", email, null)); //intent koji se korist za slanje mejla
+                            emailIntent.putExtra(Intent.EXTRA_TEXT, tvSadrzajRecepta.getText().toString()); //body tog maila
+                            emailIntent.putExtra(Intent.EXTRA_SUBJECT, tvImeRecepta.getText().toString()); //subject maila
+                            startActivity(Intent.createChooser(emailIntent, ""));
+                            d.cancel();
+                        }
+                        Toast.makeText(RecipeDetailsActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
             }
         });
     }
